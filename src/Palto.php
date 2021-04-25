1e1631102e704190f2e784557246db3b92a24eff
Kanstantsin <haspadar@gmail.com>
2021-04-25 22:13:22 +0300

    Added logger, static instance

--- src/Palto.php
+++ src/Palto.php
@@ -2,20 +2,18 @@
 
 namespace Palto;
 
-use JetBrains\PhpStorm\Pure;
+use Dotenv\Dotenv;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 
 class Palto
 {
+    private static Palto $instance;
     private string $title;
-
     private string $h1;
-
     private string $description;
-
     private string $previousPageUrl;
-
     private string $nextPageUrl = '';
-
     private \MeekroDB $db;
     private string $regionUrl = '';
     private string $categoryUrl = '';
@@ -23,14 +21,25 @@ class Palto
     private ?array $region;
     private ?array $category;
     private ?array $ad;
-
-    private string $layoutDirectory = 'layouts/';
-
+    private string $layoutDirectory = '../layouts/';
     private int $pageNumber = 1;
+    private array $env;
+    private Logger $logger;
 
-    public function __construct(private string $url, private array $env)
+    public function __construct(private string $rootDirectory = '', private string $url = '')
     {
-        $this->initDb($env);
+        if (!$rootDirectory) {
+            $this->initRootDirectory();
+        }
+
+        if (!$this->url) {
+            $this->initUrl();
+        }
+
+        $dotenv = Dotenv::createImmutable($this->rootDirectory);
+        $this->env = $dotenv->load();
+        $this->initLogger();
+        $this->initDb();
         $this->initRegionUrl();
         $this->initCategoryUrl();
         $this->initAdId();
@@ -38,6 +47,12 @@ class Palto
         $this->initCategory();
         $this->initPageNumber();
         $this->initAd();
+        self::$instance = $this;
+    }
+
+    public static function getInstance(): Palto
+    {
+        return self::$instance;
     }
 
     public function getLayout(): string
@@ -67,77 +82,9 @@ class Palto
         return $this->region;
     }
 
-    private function initRegionUrl()
-    {
-        $parts = $this->getUrlParts();
-        $this->regionUrl = $parts[1] ?? '';
-    }
-
-    private function initRegion()
-    {
-        if ($this->regionUrl) {
-            $this->region = $this->db->queryFirstRow('SELECT * FROM regions WHERE url = %s', $this->regionUrl);
-        }
-    }
-
-    private function initPageNumber()
-    {
-        $parts = $this->getUrlParts();
-        $lastPart = $parts[count($parts) - 1];
-        if ($this->isPageUrlPart($lastPart)) {
-            $this->pageNumber = intval($lastPart);
-        }
-    }
-
-    private function initCategoryUrl()
-    {
-        $parts = $this->getUrlParts();
-        if (count($parts) > 2) {
-            $lastPart = $parts[count($parts) - 1];
-            if ($this->isPageUrlPart($lastPart) || $this->isAdUrlPart($lastPart)) {
-                $this->categoryUrl = $parts[count($parts) - 2] ?? '';
-            } else {
-                $this->categoryUrl = $lastPart;
-            }
-        }
-    }
-
-    private function initCategory()
-    {
-        if ($this->categoryUrl) {
-            $this->category = $this->db->queryFirstRow('SELECT * FROM categories WHERE url = %s', $this->categoryUrl);
-        }
-    }
-
     public function getCategory(): ?array
     {
         return $this->category;
-    }
-
-    private function isPageUrlPart(string $urlPart): bool
-    {
-        return is_numeric($urlPart);
-    }
-
-    private static function isAdUrlPart(string $urlPart): bool
-    {
-        return substr($urlPart, 0, 2) == 'ad';
-    }
-
-    private function initAdId()
-    {
-        $parts = $this->getUrlParts();
-        $lastPart = $parts[count($parts) - 1];
-        if ($this->isAdUrlPart($lastPart)) {
-            $this->adId = intval(substr($lastPart, 2));
-        }
-    }
-
-    private function initAd()
-    {
-        if ($this->adId) {
-            $this->ad = $this->db->queryFirstRow('SELECT * FROM ads WHERE id = %s', $this->adId);
-        }
     }
 
     /**
@@ -156,12 +103,14 @@ class Palto
         $this->layoutDirectory = $layoutDirectory;
     }
 
-    /**
-     * @param array $env
-     */
-    private function initDb(array $env): void
+    private function initDb(): void
     {
-        $this->db = new \MeekroDB('localhost', $env['DB_USER'], $env['DB_PASSWORD'], $env['DB_NAME']);
+        $this->db = new \MeekroDB(
+            'localhost',
+            $this->env['DB_USER'],
+            $this->env['DB_PASSWORD'],
+            $this->env['DB_NAME']
+        );
     }
 
     /**
@@ -195,9 +144,118 @@ class Palto
     {
         return $this->adId;
     }
-    
+
+    /**
+     * @return null[]|string[]
+     */
+    public function getEnv(): array
+    {
+        return $this->env;
+    }
+
+    public function getLogger(): Logger
+    {
+        return $this->logger;
+    }
+
+    /**
+     * @return \MeekroDB
+     */
+    public function getDb(): \MeekroDB
+    {
+        return $this->db;
+    }
+
     private function getUrlParts(): array
     {
         return array_values(array_filter(explode('/', $this->url)));
+    }
+
+    private function initRootDirectory()
+    {
+        if (php_sapi_name() === 'cli') {
+            $this->rootDirectory = $_SERVER['PWD'] ?? '';
+        } else {
+            $this->rootDirectory = dirname($_SERVER['DOCUMENT_ROOT']);
+        }
+    }
+
+    private function initUrl()
+    {
+        $this->url = $_SERVER['REQUEST_URI'] ?? '/';
+    }
+
+    private function isPageUrlPart(string $urlPart): bool
+    {
+        return is_numeric($urlPart);
+    }
+
+    private static function isAdUrlPart(string $urlPart): bool
+    {
+        return substr($urlPart, 0, 2) == 'ad';
+    }
+
+    private function initAdId()
+    {
+        $parts = $this->getUrlParts();
+        $lastPart = $parts[count($parts) - 1] ?? '';
+        if ($this->isAdUrlPart($lastPart)) {
+            $this->adId = intval(substr($lastPart, 2));
+        }
+    }
+
+    private function initAd()
+    {
+        if ($this->adId) {
+            $this->ad = $this->db->queryFirstRow('SELECT * FROM ads WHERE id = %s', $this->adId);
+        }
+    }
+
+    private function initRegionUrl()
+    {
+        $parts = $this->getUrlParts();
+        $this->regionUrl = $parts[1] ?? '';
+    }
+
+    private function initRegion()
+    {
+        if ($this->regionUrl) {
+            $this->region = $this->db->queryFirstRow('SELECT * FROM regions WHERE url = %s', $this->regionUrl);
+        }
+    }
+
+    private function initPageNumber()
+    {
+        $parts = $this->getUrlParts();
+        $lastPart = $parts[count($parts) - 1] ?? '';
+        if ($this->isPageUrlPart($lastPart)) {
+            $this->pageNumber = intval($lastPart);
+        }
+    }
+
+    private function initCategoryUrl()
+    {
+        $parts = $this->getUrlParts();
+        if (count($parts) > 2) {
+            $lastPart = $parts[count($parts) - 1] ?? '';
+            if ($this->isPageUrlPart($lastPart) || $this->isAdUrlPart($lastPart)) {
+                $this->categoryUrl = $parts[count($parts) - 2] ?? '';
+            } else {
+                $this->categoryUrl = $lastPart;
+            }
+        }
+    }
+
+    private function initCategory()
+    {
+        if ($this->categoryUrl) {
+            $this->category = $this->db->queryFirstRow('SELECT * FROM categories WHERE url = %s', $this->categoryUrl);
+        }
+    }
+
+    private function initLogger()
+    {
+        $this->logger = new Logger('palto');
+        $this->logger->pushHandler(new StreamHandler($this->getEnv()['LOG_PATH']));
     }
 }

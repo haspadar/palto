29aa9c001caabc784a87a8c4224514f907fc916a
Kanstantsin <haspadar@gmail.com>
2021-05-01 23:03:34 +0300

    added categories in 404 layout

--- structure/layouts/404.php
+++ structure/layouts/404.php
@@ -1,2 +1,30 @@
 <?php
-echo '404';
+
+/**
+ * @var $this \Palto\Palto
+ */
+$this->partial('header.inc', [
+    'title' => '404',
+    'description' => '404',
+]);
+?>
+    <br/>
+    <h1>Not found</h1>
+
+    <h2>Categories</h2>
+<?php foreach ($this->getCategories(0, 1) as $level1Category) :?>
+    <div class="span-d">
+        <p>
+            <a href="<?=$this->generateCategoryUrl($level1Category)?>">
+                <?php if ($level1Category['icon_url']) :?>
+                    <img src="<?=$level1Category['icon_url']?>"
+                         title="<?=$level1Category['icon_text']?>"
+                         class="icm" />
+                <?php endif?>
+                <strong> <?=$level1Category['title']?></strong>
+            </a>
+        </p>
+    </div>
+<?php endforeach;?>
+
+<?php $this->partial('footer.inc', []);

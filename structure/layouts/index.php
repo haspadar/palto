b148fe68d13327afe6b6df7826646af9f567040b
Kanstantsin <haspadar@gmail.com>
2021-05-01 20:58:18 +0300

    added regions in index layout

--- structure/layouts/index.php
+++ structure/layouts/index.php
@@ -9,7 +9,29 @@ $this->partial('header.inc', [
 ]);
 ?>
     <br/>
-    <!--3rows-->
+    <h2>Regions</h2>
+    <?php foreach ($this->getRegions(0, 1) as $level1Region) :?>
+        <div class="span-d">
+            <p>
+                <a href="<?=$this->generateRegionUrl($level1Region)?>">
+                    <strong> <?=$level1Region['title']?></strong>
+                </a>
+            </p>
+            <?php if ($level2Regions = $this->getRegions($level1Region['id'])) :?>
+                <ul>
+                    <?php foreach ($level2Regions as  $level2Region) :?>
+                        <li><a href="<?=$this->generateRegionUrl($level2Region)?>">
+                                <?=$level2Region['title']?>
+                            </a>
+                        </li>
+                    <?php endforeach;?>
+                </ul>
+            <?php endif;?>
+        </div>
+    <?php endforeach;?>
+
+    <br style="clear: both">
+    <h2>Categories</h2>
     <?php foreach ($this->getCategories(0, 1) as $level1Category) :?>
         <div class="span-d">
             <p>

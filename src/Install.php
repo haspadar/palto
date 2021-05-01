c642b0af7ed8347b812262587501923bb292091f
Kanstantsin <haspadar@gmail.com>
2021-05-02 00:36:56 +0300

    fixed cron command exists checking

--- src/Install.php
+++ src/Install.php
@@ -177,15 +177,17 @@ class Install
     private function updateCron()
     {
         $cronFilePath = '/etc/crontab';
-        $command = '0 * * * *  root cd ' . $this->projectPath . ' && php ' . Status::PARSE_ADS_SCRIPT;
+        $timePart = '0 * * * *  ';
+        $commandPart = 'root cd ' . $this->projectPath . ' && php ' . Status::PARSE_ADS_SCRIPT;
+        $everyHourCommand = $timePart . $commandPart;
         $cronFileContent = file_get_contents($cronFilePath);
-        $isCommandExists = mb_strpos($command, Status::PARSE_ADS_SCRIPT) !== false;
+        $isCommandExists = mb_strpos($cronFileContent, $commandPart) !== false;
         if (!$isCommandExists) {
-            file_put_contents($cronFilePath, $cronFileContent . PHP_EOL . '#Every hour' . PHP_EOL . $command);
-            $this->log('Added cron command "' . $command . '"');
+            file_put_contents($cronFilePath, $cronFileContent . PHP_EOL . '#Every hour' . PHP_EOL . $everyHourCommand);
+            $this->log('Added cron command "' . $everyHourCommand . '"');
             $this->runCommands(['service cron reload']);
         } else {
-            $this->log('cron command for script "' . $command . Status::PARSE_ADS_SCRIPT . '" already exists');
+            $this->log('cron command for script "' . Status::PARSE_ADS_SCRIPT . '" already exists');
         }
     }
 

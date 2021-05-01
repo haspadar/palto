54c6f47b95c5eeefcdade2f4c129aaf98c76393e
Kanstantsin <haspadar@gmail.com>
2021-05-01 22:39:40 +0300

    added auth checking in index.php

--- structure/public/index.php
+++ structure/public/index.php
@@ -5,4 +5,8 @@ use Palto\Palto;
 require_once '../vendor/autoload.php';
 
 $palto = new Palto();
+if ($palto->getEnv()['AUTH']) {
+    $palto->checkAuth();
+}
+
 $palto->loadLayout($palto->getLayout());

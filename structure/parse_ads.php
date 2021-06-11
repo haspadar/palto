fe8ce0d309f0f4942e6015d067b92a0bc6e0a9d9
Kanstantsin <haspadar@gmail.com>
2021-06-11 15:17:47 +0300

    added scheduller pylesos exception callback support

--- structure/parse_ads.php
+++ structure/parse_ads.php
@@ -15,31 +15,30 @@ $palto = new Palto();
 $pid = Status::getParserPid(Status::PARSE_ADS_SCRIPT);
 $palto->getLogger()->info('Started ads parsing with pid=' . $pid);
 $scheduler = new Scheduler($palto->getEnv());
-try {
-    $scheduler->run(
-        function () use ($palto, $pid) {
-            $leafCategories = $palto->getDb()->query(
-                "SELECT * FROM categories WHERE id NOT IN (SELECT parent_id FROM categories WHERE parent_id IS NOT NULL)"
-            );
-            if ($leafCategories) {
-                shuffle($leafCategories);
-                $leafCategoriesCount = count($leafCategories);
-                foreach ($leafCategories as $leafKey => $category) {
-                    $logContent = [
-                        'iteration' => ($leafKey + 1) . '/' . $leafCategoriesCount,
-                        'pid' => $pid
-                    ];
-                    $palto->getLogger()->info('Parsing category ' . $category['title'], $logContent);
-                    parseCategory($palto, $category, $category['donor_url'], $logContent);
-                }
-            } else {
-                $palto->getLogger()->info('Categories not found');
+$scheduler->run(
+    function () use ($palto, $pid) {
+        $leafCategories = $palto->getDb()->query(
+            "SELECT * FROM categories WHERE id NOT IN (SELECT parent_id FROM categories WHERE parent_id IS NOT NULL)"
+        );
+        if ($leafCategories) {
+            shuffle($leafCategories);
+            $leafCategoriesCount = count($leafCategories);
+            foreach ($leafCategories as $leafKey => $category) {
+                $logContent = [
+                    'iteration' => ($leafKey + 1) . '/' . $leafCategoriesCount,
+                    'pid' => $pid
+                ];
+                $palto->getLogger()->info('Parsing category ' . $category['title'], $logContent);
+                parseCategory($palto, $category, $category['donor_url'], $logContent);
             }
+        } else {
+            $palto->getLogger()->info('Categories not found');
         }
-    );
-} catch (\Exception $e) {
-    $palto->getLogger()->error($e->getMessage());
-}
+    },
+    function(Exception $e) use ($palto) {
+        $palto->getLogger()->error($e->getMessage());
+    }
+);
 
 function parseCategory(Palto $palto, array $category, string $url, array $logContent = [])
 {

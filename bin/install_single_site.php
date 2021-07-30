bb3ae2f6d39ee5df8be368a27c946d16e42b2a0c
Kanstantsin <haspadar@gmail.com>
2021-07-30 15:14:47 +0300

    added palto other directories search

--- bin/install_single_site.php
+++ bin/install_single_site.php
@@ -6,4 +6,4 @@ use Palto\Install;
 require_once 'vendor/autoload.php';
 
 $install = new Install();
-$install->run(\Palto\Palto::PARSE_MANY_SITES_ADS_SCRIPT);
+$install->run(\Palto\Palto::PARSE_SINGLE_SITE_ADS_SCRIPT);

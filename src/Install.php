cfda37f3de0168f102a6bfdfe4bf4b3aa8ca8752
Kanstantsin <haspadar@gmail.com>
2021-10-03 19:37:40 +0300

    removed unique cron command checking

--- src/Install.php
+++ src/Install.php
@@ -219,31 +219,14 @@ class Install
     private function updateCron(string $parseAdsScript)
     {
         $cronFilePath = '/etc/crontab';
-        $serverCommands = [
-            '#Every 30 minutes reindex sphinx' => '*/30 * * * *  root cd ' . $this->projectPath . '/sphinx && php ' . Search::REINDEX_SCRIPT
-        ];
-        foreach ($serverCommands as $comment => $serverCommand) {
-            $cronFileContent = file_get_contents($cronFilePath);
-            $isCommandExists = mb_strpos($cronFileContent, $comment) !== false;
-            if (!$isCommandExists) {
-                file_put_contents(
-                    $cronFilePath,
-                    $cronFileContent . PHP_EOL . $comment . PHP_EOL . $serverCommand . PHP_EOL
-                );
-                $this->log('Added cron command "' . $serverCommand . '"');
-                $this->runCommands(['service cron reload']);
-            } else {
-                $this->log('cron command "' . $serverCommand . '" already exists');
-            }
-        }
-
-        $projectCommands = [
+        $commands = [
             '#Every hour' => [
                 '0 * * * *  root cd ' . $this->projectPath . ' && php ' . $parseAdsScript,
             ],
             '#Every day' => ['0 1 * * *  root cd ' . $this->projectPath . ' && php ' . Sitemap::GENERATE_SCRIPT],
+            '#Every 30 minutes' => '*/30 * * * *  root cd ' . $this->projectPath . '/sphinx && php ' . Search::REINDEX_SCRIPT
         ];
-        foreach ($projectCommands as $comment => $commentCommands) {
+        foreach ($commands as $comment => $commentCommands) {
             foreach ($commentCommands as $command) {
                 $cronFileContent = file_get_contents($cronFilePath);
                 $isCommandExists = mb_strpos($cronFileContent, $command) !== false;

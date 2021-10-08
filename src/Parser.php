43f19401a474d4d90b685f325777c1a70c37dd17
Kanstantsin <haspadar@gmail.com>
2021-10-08 17:33:57 +0300

    removed floatval in parser price

--- src/Parser.php
+++ src/Parser.php
@@ -17,7 +17,7 @@ class Parser
 
     public static function filterPrice(string $price): float
     {
-        $filtered = floatval(strtr($price, [',' => '', ' ' => '']));
+        $filtered = strtr($price, [',' => '', ' ' => '']);
 
         return min($filtered, 99999999.99);
     }

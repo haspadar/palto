da7692bb8b884b594ff87cc98f6a96e92de8e64e
Kanstantsin <haspadar@gmail.com>
2022-01-07 23:10:15 +0300

    Added FlockStore to CheckTasks

--- tasks/CheckTasks.php
+++ tasks/CheckTasks.php
@@ -1,11 +1,12 @@
 <?php
 
 use Crunz\Schedule;
+use Symfony\Component\Lock\Store\FlockStore;
 
 $schedule = new Schedule();
 $task = $schedule->run('composer update && vendor/bin/phinx migrate');
 $task
     ->description('Composer update && Phinx migrate')
-    ->preventOverlapping();
+    ->preventOverlapping(new FlockStore(__DIR__ . '/locks'));
 
 return $schedule;

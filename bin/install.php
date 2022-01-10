7e31b65a8619cbfff25e065d4eb61dcce9886fc9
Kanstantsin <haspadar@gmail.com>
2022-01-10 23:05:35 +0300

    Added composer firectory exists in install sceipt

--- bin/install.php
+++ bin/install.php
@@ -4,7 +4,14 @@
 use Palto\Cli;
 use Palto\Install;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+$autoloadFile = __DIR__ . '/../vendor/autoload.php';
+if (!is_dir($autoloadFile)) {
+    echo 'Run `composer update first`' . PHP_EOL;
+
+    exit;
+}
+
+require_once $autoloadFile;
 
 Cli::checkSudo();
 Install::run();

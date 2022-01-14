1291e5aeb494330a8bff44c083cb432140f88e06
Kanstantsin <haspadar@gmail.com>
2022-01-14 14:25:43 +0300

    added remove regions duplicates migration

--- db/migrations/20220114112511_regions_categories_duplicates_remove_three.php
+++ db/migrations/20220114112511_regions_categories_duplicates_remove_three.php
@@ -5,6 +5,17 @@ use Phinx\Migration\AbstractMigration;
 
 final class RegionsCategoriesDuplicatesRemoveThree extends AbstractMigration
 {
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
     public function change(): void
     {
         $regionsOriginals = $this->fetchAll("select * from regions where url not REGEXP '[[:digit:]]$'");

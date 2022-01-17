69ef5e971084c4c3c1fce3a5103fc9bd626da578
Kanstantsin <haspadar@gmail.com>
2022-01-17 17:25:58 +0300

    Added no complaints message

--- layouts/karman/complaints/index.php
+++ layouts/karman/complaints/index.php
@@ -8,37 +8,41 @@ $this->partial('header.inc', [
 ]);
 ?>
 
-<table class="table table-striped">
-    <thead>
-    <tr>
-        <th>Сообщение</th>
-        <th>Дата</th>
-    </tr>
-    </thead>
-    <tbody>
-
-    <?php $actualComplaints = \Palto\Complaints::getActualComplaints()?>
-    <?php foreach ($actualComplaints as $actualComplaint) :?>
+<?php $actualComplaints = \Palto\Complaints::getActualComplaints()?>
+<?php if ($actualComplaints) :?>
+    <table class="table table-striped">
+        <thead>
         <tr>
-            <td>
-                <a href="/karman/complaints?id=<?=$actualComplaint['id']?>" class="text-decoration-none">
-                    <?php $message = $actualComplaint['message']?>
-                    <?=\Palto\Filter::shortText($message, 50)?>
-                </a>
-            </td>
-            <td>
-                <?php $createTime = new DateTime($actualComplaint['create_time'])?>
-                <?=$createTime->format('d')?> <?=\Palto\Time::russianMonth($createTime->format('m'))?>
-                <small class="text-muted"><?=$createTime->format('H:i')?></small>
-            </td>
-
+            <th>Сообщение</th>
+            <th>Дата</th>
         </tr>
-    <?php endforeach;?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
 
+        <?php foreach ($actualComplaints as $actualComplaint) :?>
+            <tr>
+                <td>
+                    <a href="/karman/complaints?id=<?=$actualComplaint['id']?>" class="text-decoration-none">
+                        <?php $message = $actualComplaint['message']?>
+                        <?=\Palto\Filter::shortText($message, 50)?>
+                    </a>
+                </td>
+                <td>
+                    <?php $createTime = new DateTime($actualComplaint['create_time'])?>
+                    <?=$createTime->format('d')?> <?=\Palto\Time::russianMonth($createTime->format('m'))?>
+                    <small class="text-muted"><?=$createTime->format('H:i')?></small>
+                </td>
 
-<button type="button" class="btn btn-warning ignore-all-complaints" data-ids="<?=implode(',', array_column($actualComplaints, 'id'))?>">Игнорировать все жалобы</button>
-<button type="button" class="btn btn-danger remove-all-ads" data-ids="<?=implode(',', array_column($actualComplaints, 'id'))?>">Удалить все анкеты</button>
+            </tr>
+        <?php endforeach;?>
+        </tbody>
+    </table>
+    <button type="button" class="btn btn-warning ignore-all-complaints" data-ids="<?=implode(',', array_column($actualComplaints, 'id'))?>">Игнорировать все жалобы</button>
+    <button type="button" class="btn btn-danger remove-all-ads" data-ids="<?=implode(',', array_column($actualComplaints, 'id'))?>">Удалить все анкеты</button>
+<?php else :?>
+    <div class="alert alert-light" role="alert">
+        Жалоб нет
+    </div>
+<?php endif;?>
 
 <?php $this->partial('footer.inc');

99d93692ba86037162197bfe9d6f506812e77eb6
Kanstantsin <haspadar@gmail.com>
2022-01-19 19:08:40 +0300

    updated migration

--- db/migrations/20220118133735_translates.php
+++ db/migrations/20220118133735_translates.php
@@ -1,6 +1,9 @@
 <?php
 declare(strict_types=1);
 
+use Dotenv\Dotenv;
+use Palto\Cli;
+use Palto\Directory;
 use Phinx\Migration\AbstractMigration;
 
 final class Translates extends AbstractMigration
@@ -24,5 +27,35 @@ final class Translates extends AbstractMigration
             'Copy Pylesos env' => \Palto\Cli::safeCopyPylesosEnv()
         ]);
         \Palto\Update::run();
+        $this->reGenerateEnv(
+            Directory::getStructureConfigsDirectory(),
+            '.pylesos',
+            Directory::getConfigsDirectory(),
+            Dotenv::createUnsafeMutable(Directory::getConfigsDirectory(), '.env')->load()
+        );
+        $this->reGenerateEnv(
+            Directory::getStructureConfigsDirectory(),
+            '.env',
+            Directory::getConfigsDirectory(),
+            Dotenv::createUnsafeMutable(Directory::getConfigsDirectory(), '.env')->load()
+        );
+    }
+
+    private function reGenerateEnv(string $structureConfigDirectory, string $envFileName, string $configDirectoryDirectory, array $fullEnv)
+    {
+        $replaces = [];
+        $structureEnv = Dotenv::createUnsafeMutable($structureConfigDirectory, $envFileName)->load();
+        foreach ($fullEnv as $key => $value) {
+            if (isset($structureEnv[$key]) && strlen($value)) {
+                $replaces[$key . '="' . $structureEnv[$key] . '"'] = $key . '="' . $value . '"';
+                $replaces[$key . '=' . $structureEnv[$key]] = $key . '=' . $value;
+            }
+        }
+
+        $replaces['SUNDUK_URL=""'] = 'SUNDUK_URL="http://95.216.222.193/"';
+        $replaces['YANDEX_TRANSLATE_API_KEY='] = 'YANDEX_TRANSLATE_API_KEY="AQVNwmJ4oPONRgtb76ZwHk8tVrYWSmkMyD1vEMNu"';
+        $content = strtr(file_get_contents($structureConfigDirectory . '/' . $envFileName), $replaces);
+        \Palto\Debug::dump($content, $configDirectoryDirectory . '/' . $envFileName);
+        file_put_contents($configDirectoryDirectory . '/' . $envFileName, $content);
     }
 }

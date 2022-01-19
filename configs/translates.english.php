80e0ffaff82384f9889dcda0f72de83964d802aa
Kanstantsin <haspadar@gmail.com>
2022-01-19 16:39:16 +0300

    Added russian and english translates

---
+++ configs/translates.english.php
@@ -0,0 +1,58 @@
+<?php
+return [
+//header.inc
+    'html_lang' => 'en-US',
+//footer.inc
+    'Частные бесплатные объявления в %s' => 'Classified Ads in Washington, DC',
+    'Агрегатор всех местных досок объявлений' => 'Local classifieds board',
+    'Контакты' => 'Contacts',
+    'Текст про куки' => 'This website uses cookies to personalise content and ads, to provide social media features and to analyse our traffic. We also share information about your use of our site with our social media, advertising and analytics partners who may combine it with other information that you’ve provided to them or that they’ve collected from your use of their services.',
+    'СОГЛАСЕН' => 'ACCEPT',
+//pager.inc
+    'Следующая' => 'Next',
+    'Предыдущая' => 'Previous',
+//404.php
+    'Объявление было удалено' => 'Ad was deleted',
+    'Не найдено' => 'Not found',
+    'Категории' => 'Categories',
+//ad.php
+    'ad_h1' => ':AD <span style="color:#999"> in :ADDRESS_REGION from craigslist</span>',
+    'Показать телефон' => 'Show Phone',
+    'Нет телефона' => 'No Phone',
+    'Связаться' => 'Reply',
+    'Пожаловаться на объявление' => 'Report this ad',
+    'Жалоба' => 'Report',
+    'Ваша жалоба успешно отправлена.' => 'Your report has been sent.',
+    'Похожие объявления' => 'Similar ads',
+    'Регион' => 'Region',
+    'Время публикации' => 'Post time',
+//index.php
+    'index_title' => 'Private free classifieds in Washington from craigslist and other boards',
+    'index_description' => 'Aggregator of all classifieds boards in Washington.',
+//categories-list.php
+    'categories_description' => 'Categories of Classified Ads in Washington',
+//regions-list.php
+    'regions_title' => 'Washington Regions',
+    'regions_description' => 'Regions',
+    'regions_h1' => 'Regions',
+//list.php
+    'list_title' => 'Classified Ads in :CATEGORIES - :REGION',
+    'list_description' => 'Aggregator of all classifieds boards: :CATEGORIES :REGION',
+    'list_h1' => ':CATEGORY_IN_REGION: ads from craigslist',
+    'в' => 'in',
+//registration.php
+    'registration_title' => 'Registration',
+    'registration_description' => 'Registration',
+    'registration_h1' => 'Registration',
+    'Зарегистрировать' => 'Create account',
+    'Забыли пароль?' => 'Forgot password?',
+    'Войти' => 'Log in',
+];
+/**
+ * :AD –  название объявления
+ * :ADDRESS_WITH_REGION - адрес, регион (через запятую)
+ * :CATEGORIES – категорий через запятую
+ * :REGION – название региона
+ * :CATEGORY_IN_REGION – категория в регионе в предложном падеже или просто регион в именительном, если категории нет
+ */
+

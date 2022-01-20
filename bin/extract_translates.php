d06c1a013399e7f239ce9e6e8c932e10c73e60f3
Kanstantsin <haspadar@gmail.com>
2022-01-21 01:19:23 +0300

    Added extract translates bin

---
+++ bin/extract_translates.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php
+<?php
+
+use Palto\Counters;
+use Palto\Translates;
+
+require_once __DIR__ . '/autoload_require_composer.php';
+
+$extractedTranslates = Translates::extractTranslates();
+$isRussian = mb_substr($extractedTranslates['html_lang'], 0, 2) == 'ru';
+$fileName = \Palto\Directory::getConfigsDirectory() . ($isRussian ? '/translates.russian.php' : '/translates.english.php');
+$defaultTranslates = require_once $fileName;
+$extractedTranslates['ad_h1'] = ':AD <span style="color:#999"> ' . $defaultTranslates['в'] . ' :ADDRESS_WITH_REGION ' . ($isRussian ? 'c ' : 'from ') . \Palto\Directory::getProjectShortName();
+Translates::saveTranslates($extractedTranslates, $defaultTranslates, $fileName);
+
+$extractedCounters = $this->extractCounters();
+Counters::saveCounters($extractedCounters, \Palto\Directory::getConfigsDirectory() . '/counters.php');
--- src/Counters.php
+++ src/Counters.php
@@ -17,6 +17,40 @@ class Counters
         return $counter ?: $name;
     }
 
+    public static function extractCounters(): array
+    {
+        $patterns = [
+            'partials/footer.inc' => [
+                'liveinternet' => ['<!--LiveInternet counter-->', 0, '<!--/LiveInternet-->'],
+            ],
+            'list.php' => [
+                'google' => ['</h1>', 0, '<?php if ($flashMessage)']
+            ]
+        ];
+
+        $counters = [];
+        foreach ($patterns as $file => $fileReplaces) {
+            foreach ($fileReplaces as $translateKey => $fileReplace) {
+                if ($fileReplace) {
+                    $counters[$translateKey] = Translates::extractLayoutTranslate($fileReplace[0], $fileReplace[1], $fileReplace[2], \Palto\Directory::getLayoutsDirectory() . '/client/' . $file);
+                }
+            }
+        }
+
+        return $counters;
+    }
+
+    public static function saveCounters($extractedCounters, $fileName)
+    {
+        $content = "<?php
+return [
+    'liveinternet' => '" . ($extractedCounters['liveinternet'] ?? '') . "',
+
+    'google' => '" . ($extractedCounters['google'] ?? '') . "'
+];";
+        file_put_contents($fileName, $content);
+    }
+
     private static function getCounters(): array
     {
         if (!isset(self::$counters)) {

1a58a5638f599a76847ae398f668e9302e1f5722
Kanstantsin <haspadar@gmail.com>
2022-01-25 16:59:34 +0300

    added regions list in index layout

--- structure/layouts/client/static/index.php
+++ structure/layouts/client/static/index.php
@@ -9,6 +9,22 @@ $this->partial('header.inc', [
 ]);
 ?>
 <h1><?=$this->translate('index_h1')?></h1>
+<?php foreach ($this->getWithAdsRegions() as $level1Region) :?>
+    <div class="span-d regions"><a href="<?=$this->generateRegionUrl($level1Region)?>"><strong> <?=$level1Region->getTitle()?></strong></a>
+        <?php if ($level2Regions = $this->getWithAdsRegions($level1Region)) :?>
+            <ul>
+                <?php foreach ($level2Regions as  $level2Region) :?>
+                    <li><a href="<?=$this->generateRegionUrl($level2Region)?>"><?=$level2Region->getTitle()?></a></li>
+                <?php endforeach;?>
+            </ul>
+        <?php endif;?></div>
+<?php endforeach;?>
+
+<?=\Palto\Counters::get('google')?>
+<br style="clear: both">
+<br style="clear: both">
+<h2><?=$this->translate('Категории')?></h2>
+
 <?php foreach ($this->getWithAdsCategories() as $level1Category) :?>
     <div class="span-d">
         <p><a href="<?=$this->generateCategoryUrl($level1Category)?>">

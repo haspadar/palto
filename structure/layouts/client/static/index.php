051026b8a9ffb76befd752012ea71859d7cea91f
Kanstantsin <haspadar@gmail.com>
2022-01-27 20:41:21 +0300

    removed regions level 2 from index layout

--- structure/layouts/client/static/index.php
+++ structure/layouts/client/static/index.php
@@ -10,14 +10,9 @@ $this->partial('header.inc', [
 ?>
 <h1><?=$this->translate('index_h1')?></h1>
 <?php foreach ($this->getWithAdsRegions() as $level1Region) :?>
-    <div class="span-d regions"><a href="<?=$this->generateRegionUrl($level1Region)?>"><strong> <?=$level1Region->getTitle()?></strong></a>
-        <?php if ($level2Regions = $this->getWithAdsRegions($level1Region)) :?>
-            <ul>
-                <?php foreach ($level2Regions as  $level2Region) :?>
-                    <li><a href="<?=$this->generateRegionUrl($level2Region)?>"><?=$level2Region->getTitle()?></a></li>
-                <?php endforeach;?>
-            </ul>
-        <?php endif;?></div>
+    <div class="span-d regions">
+        <a href="<?=$this->generateRegionUrl($level1Region)?>"><strong> <?=$level1Region->getTitle()?></strong></a>
+    </div>
 <?php endforeach;?>
 
 <?=\Palto\Counters::get('google')?>

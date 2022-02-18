2bfc6d35e32ca2933374c6918948ffecc67644a2
Kanstantsin <haspadar@gmail.com>
2022-02-18 19:47:56 +0300

    Updated composer

---
+++ bin/portnoy.php
@@ -0,0 +1,10 @@
+#!/usr/bin/php
+<?php
+
+use Palto\Cli;
+use Palto\Install;
+
+require_once __DIR__ . '/autoload_require_composer.php';
+
+Cli::checkSudo();
+Portnoy::run();
---
+++ src/Portnoy.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Palto;
+
+class Portnoy
+{
+    public static function run()
+    {
+        $prompted = self::prompt();
+        $databaseName = str_replace('.', '_', Directory::getProjectName());
+        $databaseUsername = $databaseName;
+        $databasePassword = Cli::generatePassword();
+        Cli::runCommands([
+            'Update Nginx' => Cli::updateNginx(),
+            Cli::updateNginxDomain($databaseName),
+            Cli::safeLinkNginxDomain(),
+            'Update FPM' => Cli::updateNginxPhpFpm(),
+            Cli::reloadNginx(),
+            'Copy Parse Scripts' => Cli::safeCopyParseScripts(),
+            'Copy Translates' => Cli::safeCopyTranslates(),
+            'Copy Counters' => Cli::safeCopyCounters(),
+            'Copy CSS' => Cli::safeCopyCss(),
+            'Create Database' => Cli::createDatabase($databaseName, $databaseUsername, $databasePassword),
+            'Add Cron' => Cli::safeAddCron(),
+            'Generate general env' => Cli::generateGeneralEnv($databaseName, $databaseUsername, $databasePassword),
+            'Copy pylesos env' => Cli::safeCopyPylesosEnv(),
+            'Copy layouts env' => Cli::safeCopyLayoutsEnv(),
+            'Update Phinx' => Cli::updatePhinx($databaseName, $databaseUsername, $databasePassword),
+            'Update Htpasswd' => Cli::updateHtpasswd(),
+            'Update Host' => Cli::updateHost(),
+            'Update permissions' => Cli::updatePermissions(Directory::getRootDirectory()),
+            Cli::updatePermissions('/etc/nginx/sites-available')
+        ]);
+    }
+
+    private static function prompt()
+    {
+
+    }
+}

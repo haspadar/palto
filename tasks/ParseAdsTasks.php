6e68911db8d3d074fe736314b6ca2775c6c62ba3
Kanstantsin <haspadar@gmail.com>
2022-03-25 16:21:35 +0300

    added debug

--- tasks/ParseAdsTasks.php
+++ tasks/ParseAdsTasks.php
@@ -7,14 +7,6 @@ use Symfony\Component\Lock\Store\FlockStore;
 
 $schedule = new Schedule();
 
-\Palto\Logger::debug('Configs directory: ' . Directory::getConfigsDirectory());
-$env = array_merge(
-    Dotenv::createImmutable(Directory::getConfigsDirectory(), '.env')->load(),
-    Dotenv::createImmutable(Directory::getConfigsDirectory(), '.pylesos')->load(),
-    Dotenv::createImmutable(Directory::getConfigsDirectory(), '.layouts')->load(),
-);
-\Palto\Logger::debug($env);
-
 $task = $schedule->run(PHP_BINARY . ' ' . Directory::getParseAdsScript());
 $task
     ->everyMinute()

af9fe3d07a846aa88b879252fb2aebbfb8f437f6
Kanstantsin <haspadar@gmail.com>
2022-05-10 13:56:11 +0300

    Changed task time

--- tasks/FindAndMoveTasks.php
+++ tasks/FindAndMoveTasks.php
@@ -8,7 +8,7 @@ $schedule = new Schedule();
 
 $task = $schedule->run(PHP_BINARY . ' ' . \Palto\Synonyms::FIND_AND_MOVE_SCRIPT);
 $task
-    ->hourly()
+    ->everyThirtyMinutes()
     ->description('Find and move undefined and')
     ->preventOverlapping(new FlockStore(__DIR__ . '/locks'));
 

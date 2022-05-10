6d78665fa48106767f64fafcc5d4539b2fc79822
Kanstantsin <haspadar@gmail.com>
2022-05-10 22:00:35 +0300

    Removed debug

--- bin/refind_and_move.php
+++ bin/refind_and_move.php
@@ -5,12 +5,6 @@ use Palto\Categories;
 
 require_once __DIR__ . '/autoload_require_composer.php';
 
-$synonym =\Palto\Synonyms::getByTitle('golden retriever');
-\Palto\Debug::dump($synonym, '$synonym');
-$ad =\Palto\Ads::getById(115526);
-\Palto\Debug::dump($ad, '$ad');
-\Palto\Debug::dump(\Palto\Synonyms::hasAdSynonyms($ad, [$synonym], 'title'));
-exit;
 if (Categories::getUndefinedAll()) {
     \Palto\Synonyms::findAndMoveAds(Categories::getLiveCategories());
 } else {

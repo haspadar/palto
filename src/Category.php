aaaf2eb3447c6f8da94f2b3ebab78de22c78b5fd
Kanstantsin <haspadar@gmail.com>
2022-05-14 21:00:09 +0300

    Added debug

--- src/Category.php
+++ src/Category.php
@@ -46,6 +46,7 @@ class Category
 
     public function getSynonyms(): array
     {
+        Debug::dump(Synonyms::getAll($this->getId()), 'Synonyms::getAll($this->getId())');
         return array_map(fn($synonym) => new Synonym($synonym), Synonyms::getAll($this->getId()));
     }
 

f9a67a81bd01225108edbb34a015f6d3cc55b084
Kanstantsin <haspadar@gmail.com>
2022-05-14 23:30:46 +0300

    added debug

--- src/Logs.php
+++ src/Logs.php
@@ -46,10 +46,9 @@ class Logs
             $file = file($file);
             $lines = [];
             for ($i = max(0, count($file) - $limit - 1); $i < count($file); $i++) {
-                Debug::dump(count($file) - $limit + $i, 'min(count($file) - $limit + $i');
                 $lines[min(count($file) - $limit + $i, 1)] = $file[$i];
             }
-
+Debug::dump($lines);
             return $lines;
         }
 

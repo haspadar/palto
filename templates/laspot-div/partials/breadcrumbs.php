4ee13a7c012caf98d54005a07808ebc4fe254fdc
Kanstantsin <haspadar@gmail.com>
2022-06-02 20:31:55 +0300

    Fixed laspot-div breadcrumbs

--- templates/laspot-div/partials/breadcrumbs.php
+++ templates/laspot-div/partials/breadcrumbs.php
@@ -1,21 +1,20 @@
 <div class="city" itemscope itemtype="http://schema.org/BreadcrumbList">
-    <?php foreach ($this->data['breadcrumbs'] as $breadcrumbKey => $breadcrumbItem) :?>
-        <div class="city__content" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-
-            <?php if ($breadcrumbItem['url']) :?>
-                <a itemprop="item" href="<?=$breadcrumbItem['url']?>" class="bread_link">
+    <div class="city__content">
+        <?php foreach ($this->data['breadcrumbs'] as $breadcrumbKey => $breadcrumbItem) :?>
+            <span itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+                <?php if ($breadcrumbItem['url']) :?>
+                    <a itemprop="item" href="<?=$breadcrumbItem['url']?>">
+                        <span itemprop="name"><?= $breadcrumbItem['title'] ?></span>
+                    </a>
+                <?php else :?>
                     <span itemprop="name"><?= $breadcrumbItem['title'] ?></span>
-                </a>
-            <?php else :?>
-                <span itemprop="name"><?= $breadcrumbItem['title'] ?></span>
+                <?php endif;?>
+                <meta itemprop="position" content="<?=$breadcrumbKey + 1?>"/>
+
+            <?php $isLast = !isset($this->data['breadcrumbs'][$breadcrumbKey + 1]);?>
+            <?php if (!$isLast) :?>
+                <span>»</span>
             <?php endif;?>
-            <meta itemprop="position" content="<?=$breadcrumbKey + 1?>"/>
-
-        </div>
-
-        <?php $isLast = !isset($this->data['breadcrumbs'][$breadcrumbKey + 1]);?>
-        <?php if (!$isLast) :?>
-            <span class="sep">»</span>
-        <?php endif;?>
-    <?php endforeach;?>
+        <?php endforeach;?>
+    </span>
 </div>

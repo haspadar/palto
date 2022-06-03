6cf529715b39e8519e3bd668796339dc10405ddb
Kanstantsin <haspadar@gmail.com>
2022-06-03 14:52:04 +0300

    Added h2 in list template

--- src/Model/Regions.php
+++ src/Model/Regions.php
@@ -20,31 +20,27 @@ class Regions extends NestedSet
 
     public function getLiveRegions(?Region $parentRegion, int $limit = 0, $offset = 0, $orderBy = ''): array
     {
-        if (!$parentRegion || $parentRegion->getLevel() < 2) {
-            $query = 'SELECT * FROM ' . $this->name;
-            $values = [];
-            $level = $parentRegion && $parentRegion->getId() ? $parentRegion->getLevel() + 1 : 1;
-            $adsFieldRegion = "region_level_{$level}_id";
-            $query .= " WHERE level=$level AND id IN (SELECT DISTINCT $adsFieldRegion FROM ads)";
-            if ($parentRegion) {
-                $query .= ' AND parent_id = %d_parent_id';
-                $values['parent_id'] = $parentRegion->getId();
-            }
-
-            if ($orderBy) {
-                $query .= ' ORDER BY ' .$orderBy;
-            }
-
-            if ($limit) {
-                $query .= ' LIMIT %d_limit OFFSET %d_offset';
-                $values['limit'] = $limit;
-                $values['offset'] = $offset;
-            }
-
-            return self::getDb()->query($query, $values);
-        } else {
-            return [];
+        $query = 'SELECT * FROM ' . $this->name;
+        $values = [];
+        $level = $parentRegion && $parentRegion->getId() ? $parentRegion->getLevel() + 1 : 1;
+        $adsFieldRegion = "region_level_{$level}_id";
+        $query .= " WHERE level=$level AND id IN (SELECT DISTINCT $adsFieldRegion FROM ads)";
+        if ($parentRegion) {
+            $query .= ' AND parent_id = %d_parent_id';
+            $values['parent_id'] = $parentRegion->getId();
         }
+
+        if ($orderBy) {
+            $query .= ' ORDER BY ' .$orderBy;
+        }
+
+        if ($limit) {
+            $query .= ' LIMIT %d_limit OFFSET %d_offset';
+            $values['limit'] = $limit;
+            $values['offset'] = $offset;
+        }
+
+        return self::getDb()->query($query, $values);
     }
 
     public function rebuildAdsCount()

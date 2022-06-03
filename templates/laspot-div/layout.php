ea09ce3599924699fb10daea5dea83c401a79793
Kanstantsin <haspadar@gmail.com>
2022-06-03 14:02:25 +0300

    Added favicon

--- templates/laspot-div/layout.php
+++ templates/laspot-div/layout.php
@@ -11,6 +11,7 @@ use Palto\Categories;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="/img/favicon.ico">
     <link rel="stylesheet" href="<?= $this->asset(\Palto\Directory::getThemePublicDirectory() . '/css/style.css') ?>">
     <link rel="stylesheet" href="<?= $this->asset('/css/styles.css') ?>">
     <?php if (\Palto\Auth::isLogged()) : ?>

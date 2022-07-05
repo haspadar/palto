18265ff39bacda824249ad5963d7b23494add6b2
Kanstantsin <haspadar@gmail.com>
2022-07-05 15:43:47 +0300

    Added debug

--- src/Sitemap.php
+++ src/Sitemap.php
@@ -39,6 +39,9 @@ class Sitemap
              * @var Category[] $categories
              */
             foreach ($groupedCategories as $categories) {
+                if ($regions[0]->getUrl() == 'all') {
+                    Debug::dump($regions[0], 'all-region');exit;
+                }
                 $this->generateCategoriesFiles(
                     '/' . $regions[0]->getUrl() . '-' . $categories[0]->getUrl(),
                     $regions,

e846fad629e74039b31225cd5d5d48bdaba6cc99
Kanstantsin <haspadar@gmail.com>
2022-07-28 11:45:31 +0300

    Added ad url fix

--- db/migrations/20220728084513_ad_url_fix_2.php
+++ db/migrations/20220728084513_ad_url_fix_2.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 use Phinx\Migration\AbstractMigration;
 
-final class AdUrlFix extends AbstractMigration
+final class AdUrlFix2 extends AbstractMigration
 {
     /**
      * Change Method.
